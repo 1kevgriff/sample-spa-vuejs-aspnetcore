881aaa8a83c2d5a161528fb5af57dd8f399f4981
Kevin Griffin <kevin@kevgriffin.com>
2020-05-05 09:55:52 -0400

    updateS?

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            // TODO: write code
+            return Ok();
+        }
+    }
+}
+
+
